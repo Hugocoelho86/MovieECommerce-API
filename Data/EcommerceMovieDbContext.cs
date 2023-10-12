ee5e5a2306db47e5dc093e945e16a733a7d5bb99
Hugo Coelho <hugo.hec@gmail.com>
2023-10-12 20:19:40 +0100

    First Commit

---
+++ Data/EcommerceMovieDbContext.cs
@@ -0,0 +1,12 @@
+﻿using ECommerceMovies.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceMovies.API.Data
+{
+    public class EcommerceMovieDbContext: DbContext
+    {
+        public EcommerceMovieDbContext(DbContextOptions<EcommerceMovieDbContext> options) : base(options) { }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerceMovies.API.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public int Title { get; set; }
+        public int Category { get; set; }
+        public int ReleaseYear { get; set; }
+    }
+}
